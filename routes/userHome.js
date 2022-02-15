eef5af722f199b5f5eb97bfcf7cb750711a29e4b
christopher pipitone <Chrispipitone123@gmail.com>
2022-02-14 21:21:43 -0500

    fixed issue with userHome on client side, adjusted its server side to reflect, need to do work on req.body stuff for creating a sandwich

--- routes/userHome.js
+++ routes/userHome.js
@@ -5,12 +5,14 @@ const pool = require("../db");
 
 router.get("/", authorization, async (req, res) => {
   try {
-      ///important
+      //gets sandwiches id and name relevant to the user
         const user = await pool.query(
-            "SELECT username, user_id FROM user_profile WHERE user_id = $1",
-            [req.user] 
+            "SELECT sandwich_id, sandwich_name FROM user_sandwich WHERE uid = $1",
+            [req.user.id] 
         ); 
-        res.json(user.rows[0]);
+        
+        res.json(user.rows);
+        
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server error");
